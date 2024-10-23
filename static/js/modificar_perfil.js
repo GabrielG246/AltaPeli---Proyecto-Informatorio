39888915631efd81f07e743e614c356a2e1d5dfd
Sebastian <sebastiannmeana@gmail.com>
2024-10-23 17:02:09 -0300

    Cambios realizados en vista, template y logica para editar el perfil de usuario

---
+++ static/js/modificar_perfil.js
@@ -0,0 +1,24 @@
+function previewImage(event) {
+       const currentImage = document.getElementById('current-profile-image');
+       const newProfileImage = document.getElementById('new-profile-image');
+       
+       // Si hay un archivo seleccionado
+       if (event.target.files.length > 0) {
+           const file = event.target.files[0];
+           const reader = new FileReader();
+   
+           // Mostrar la imagen cargada
+           reader.onload = function(e) {
+               newProfileImage.src = e.target.result;
+               newProfileImage.style.display = 'block';  // Mostrar la imagen de vista previa
+               currentImage.style.display = 'none';  // Ocultar la imagen actual
+           }
+   
+           reader.readAsDataURL(file);
+       } else {
+           // Si no hay archivo seleccionado, mostrar la imagen actual
+           newProfileImage.style.display = 'none';  // Ocultar la nueva imagen
+           currentImage.style.display = 'block';  // Mostrar la imagen actual
+       }
+   }
+   
